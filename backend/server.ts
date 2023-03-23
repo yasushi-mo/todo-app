a0a63348e09522d7208eedbed82e9371e844a732
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-03-23 09:20:57 +0900

    Add:Console log

--- backend/server.ts
+++ backend/server.ts
@@ -1,3 +1,12 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
 
-const app: express.Express = express();
+const app: Express = express();
+const port = 3001;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
