ef16ac9aca305a6f04de5aaeb4d43b9d93b19cbd
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2023-03-28 09:34:04 +0900

    Add:Read todo list

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,23 @@
 import express, { Express, Request, Response } from "express";
+import { Todo } from "../types";
 
 const app: Express = express();
 const port = 3001;
 
+const sampleData = [
+  { id: 1, title: "牛乳を買う", completed: false },
+  { id: 2, title: "洗濯をする", completed: true },
+  { id: 3, title: "請求書を支払う", completed: false },
+];
+
+let todoList: Todo[] = sampleData;
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+app.get("/todo-list", (req: Request, res: Response) => {
+  res.json(todoList);
 });
 
 app.listen(port, () => {
